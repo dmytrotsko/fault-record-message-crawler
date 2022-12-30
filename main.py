810051349b5fc78b883f8ae676d2944de398309c
Dmytro Trotsko <dmytrotsko@gmail.com>
2022-12-30 17:15:51 +0200

    Removed parsing logic (it was moved to utils.py). Updated main() function with faults and faults updates posting instead of writing them to a json file.

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import os
-import cProfile
 
 import slack
 from dotenv import load_dotenv
-from tqdm import tqdm
 
-from utils import (get_conversation_history, get_message_replies,
-                   parse_user_message, write_result, parse_bot_message, logger)
+from utils import (get_conversation_history, process_conversation_history, post_fault_record, post_fault_record_updates)
 
 load_dotenv()
 
 SLACK_TOKEN = os.getenv("SLACK_TOKEN")
 CHANNEL_ID = os.getenv("SLACK_CHANNEL_ID")
 MESSAGE_LIMIT_PER_REQUEST = os.getenv("MESSAGE_LIMIT_PER_REQUEST")
+FAULT_RECORD_POST_URL = os.getenv("FAULT_RECORD_POST_URL")
+FAULT_RECORD_UPDATE_POST_URL = os.getenv("FAULT_RECORD_UPDATE_POST_URL")
 
 
 def main():
@@ -22,37 +21,11 @@ def main():
         channel_id=CHANNEL_ID,
         msg_limit=MESSAGE_LIMIT_PER_REQUEST
     )
-    result = []
-    for message in tqdm(conversation_history):
-        if message.get("subtype", "").startswith("channel_"):
-            continue
-        if message.get("subtype", "") != "bot_message":
-            parsed_message = parse_user_message(message, CHANNEL_ID, client)
-            if message.get("reply_count") is not None:
-                replies = get_message_replies(
-                    client=client,
-                    channel_id=CHANNEL_ID,
-                    parent_message_ts=message["ts"]
-                )
-                parsed_message["updates"] = replies[1:]
-        else:
-            try:
-                if "successful" in message["attachments"][0]["title"].lower():
-                    continue # To skip messages about successful runs
-                parsed_message = parse_bot_message(message, CHANNEL_ID, client)
-                if message.get("reply_count") is not None and message.get("reply_count") > 1:
-                    replies = get_message_replies(
-                        client=client,
-                        channel_id=CHANNEL_ID,
-                        parent_message_ts=message["ts"]
-                    )
-                    parsed_message["updates"] = replies
-            except KeyError:
-                logger.error(f"Could not parse bot message. Message ts: {message['ts']}")
-                continue
-        result.append(parsed_message)
-    write_result("outages-parsed-users", result)
-
+    processed_messages = process_conversation_history(conversation_history, client, CHANNEL_ID)
+    for message in processed_messages:
+        fault_id = post_fault_record(message, FAULT_RECORD_POST_URL)
+        if message.get("updates"):
+            post_fault_record_updates(message.get("updates"), fault_id, FAULT_RECORD_UPDATE_POST_URL)
 
 
 if __name__ == "__main__":
